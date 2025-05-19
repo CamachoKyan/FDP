60d0aa20b6ba76dc725684ef0572e690877bd36e
CamachoKyan <124410885+CamachoKyan@users.noreply.github.com>
2025-05-18 20:56:59 -0700

    Add files via upload

---
+++ Ejercicio3/Ejercicio3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+/*Ejercicio #3
+Made By: Kyan Camacho*/
+class Ejercicio2 {
+  public static void main(String[] args) {
+    Scanner Input = new Scanner(System.in);
+    System.out.println("Ingresa Un Primer Número:");
+    int x = Input.nextInt();
+    System.out.println("Ingresa Un Segundo Número:");
+    int y = Input.nextInt();
+
+    if(x == y){
+        System.out.println("Los Números Tienen El Mismo Valor");
+    }else if (x>y){
+        System.out.println("El Primer Número "+x+" Es Mayor Que El Segundo Número "+y);
+    }else{
+        System.out.println("El Primer Número "+x+" Es Menor Que El Segundo Número "+y);
+    }
+  }
+}
