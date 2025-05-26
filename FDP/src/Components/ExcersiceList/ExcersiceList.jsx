fc54fda8a78b16421bd80474907b81f121c42276
CamachoKyan <kyan.camacho21@cetis107.edu.mx>
2025-05-26 12:45:05 -0700

    Bobo

--- FDP/src/Components/ExcersiceList/ExcersiceList.jsx
+++ FDP/src/Components/ExcersiceList/ExcersiceList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const ExcersiceList = () => {
+export const ExcersiceList = () => {
   const [Folders, setFolders] = useState([]);
   const [Loading, setLoading] = useState(true);
 
